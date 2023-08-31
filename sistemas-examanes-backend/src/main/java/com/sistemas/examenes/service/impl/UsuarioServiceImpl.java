02cce6f5a7d921505bb2f6312f84f201df74b7bb
JavierDuranFlores <duranfloresj7@gmail.com>
2023-08-31 03:17:00 -0600

    Se crea el paquete impl y dentro de el 1 implementacion, UsuarioServiceImpl la cual es una clase e implementa de UsuarioService, y sobre escribe el metodo guardarUsuario y le damos la logica para guardar un usuario.

---
+++ sistemas-examanes-backend/src/main/java/com/sistemas/examenes/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,42 @@
+package com.sistemas.examenes.service.impl;
+
+import com.sistemas.examenes.entity.Usuario;
+import com.sistemas.examenes.entity.UsuarioRol;
+import com.sistemas.examenes.repository.RolRepository;
+import com.sistemas.examenes.repository.UsuarioRepository;
+import com.sistemas.examenes.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service("usuarioServiceImpl")
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Autowired
+    @Qualifier("usuarioRepository")
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    @Qualifier("rolRepository")
+    private RolRepository rolRepository;
+
+    @Override
+    public Usuario guardarUsuario(Usuario usuario, Set<UsuarioRol> usuarioRoles) throws Exception {
+        Usuario usuarioLocal = usuarioRepository.findByUsername(usuario.getUsername());
+
+        if (usuarioLocal != null){
+            System.out.println("El usuario ya existe");
+            throw new Exception("El usuario ya esta presente");
+        } else {
+            for (UsuarioRol usuarioRol : usuarioRoles) {
+                rolRepository.save(usuarioRol.getRol());
+            }
+            usuario.getUsuarioRoles().addAll(usuarioRoles);
+            usuarioLocal = usuarioRepository.save(usuario);
+        }
+
+        return usuarioLocal;
+    }
+}
