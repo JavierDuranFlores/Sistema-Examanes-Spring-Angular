e48812b726e76d5ea88e0db53fbf59053c74ddba
JavierDuranFlores <duranfloresj7@gmail.com>
2023-08-31 02:58:40 -0600

    Se inicia, de spring initializr se descarga el proyecto de spring y se descomprime.

---
+++ sistemas-examanes-backend/src/main/java/com/sistemas/examenes/SistemasExamanesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistemas.examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasExamanesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasExamanesBackendApplication.class, args);
+	}
+
+}
---
+++ sistemas-examanes-backend/src/test/java/com/sistemas/examenes/SistemasExamanesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemas.examenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasExamanesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
